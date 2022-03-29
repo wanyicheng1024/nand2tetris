5f38da119598de765430a2534ada8ec02d651a47
wanyicheng <102396705+wanyicheng1024@users.noreply.github.com>
2022-03-29 18:02:10 +0800

    Create Assembler.js

---
+++ 06/01/Assembler.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const parser = require('./Parser');
+
+let fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf-8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+
+    data = data.split('\r\n');
+
+    // parser([...data], true);
+
+    const binaryOut = parser(data);
+
+    fileName = fileName.split('.')[0];
+
+    fs.writeFile(fileName + '.hack', binaryOut, (err) => {
+        if (err) {
+            throw err;
+        }
+    })
+})
