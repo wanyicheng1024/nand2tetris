9db1a5ddba257e223eb92fd5ebe72e3b0fdc4bc3
wanyicheng <102396705+wanyicheng1024@users.noreply.github.com>
2022-04-01 17:06:03 +0800

    Add files via upload

---
+++ 08/02/Vmtranslator.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const parser = require('./parser');
+const code = require('./codewriter')
+const { writeInit } = code;
+
+let asmOut = '';
+let asmFileName = '';
+let fileName = process.argv[2];
+
+if (fs.lstatSync(fileName).isDirectory()) {
+    asmFileName = fileName;
+    fs.readdir(fileName, (err, files) => {
+        let initIndex = files.findIndex(file => file.indexOf('Sys.vm') > -1);
+        if (initIndex > -1) {
+            asmOut += writeInit();
+        }
+        files.forEach(file => {
+            let tempArry = file.split('.')
+            if (tempArry.pop() == 'vm') {
+                let preName = tempArry.join('.')
+                let data = fs.readFileSync(`${fileName}/${file}`, 'utf-8')
+                data = data.split('\r\n');
+                asmOut += parser(data, preName);
+            }
+        })
+        setFileName();
+    })
+} else {
+    let tempArry = fileName.split('.')
+    tempArry.pop() 
+    let preName = tempArry.join('.')
+    asmFileName = preName
+
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        data = data.split('\r\n');
+        asmOut = parser(data, preName);
+        setFileName();
+    })
+}
+
+function setFileName() {
+    fs.writeFile(asmFileName + '.asm', asmOut, (err) => {
+        if (err) {
+            throw err
+        }
+    })
+}
