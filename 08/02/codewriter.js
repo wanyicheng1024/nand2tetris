8d1f9ce669eff2f8aa97c0c59baafe8ab761c9f6
wanyicheng <102396705+wanyicheng1024@users.noreply.github.com>
2026-09-06 20:13:26 +0800

    fix codewrite bug

--- 08/02/codewriter.js
+++ 08/02/codewriter.js
@@ -521,7 +521,7 @@ function writeIf(command) {
     let label = getLabel(command);
     let out = '';
     out = '@SP \r\n'+
-        'AM = M - 1' + '\r\n' +
+        'A = M - 1' + '\r\n' +
         'D = M' + '\r\n' +
         '@' + label + '\r\n' +
         'D;JNE' + '\r\n'
